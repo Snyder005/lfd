df9918d731895412805bc3d5e37b4ddfb2d385e7
Adam Snyder <snyder005@gmail.com>
2022-08-30 10:46:50 -0700

    Added Von Karman improvements to lfd.

--- lfd/analysis/profiles/seeing.py
+++ lfd/analysis/profiles/seeing.py
@@ -55,3 +55,45 @@ class GausKolmogorov(ConvolutionObject):
             return gk(r)
         except TypeError:
             return gk(np.asarray(r))
+
+class VonKarman(ConvolutionObject):
+    """Simple Von Karman seeing, interpolated from existing points. Convolving 
+    with this function has the effect of blurring the original object.
+
+    Parameters
+    ----------
+    fwhm : float
+      FWHM of the Von Karman profile
+    scale : list, tuple or np.array
+      if scale is given then VK will be evaluated over it
+    res : float
+      if scale is not given one will be created from the estimated width of the
+      VK profile and resolution in arcseconds
+    """
+   
+    def __init__(self, fwhm, scale=None, res=0.001):
+                
+        self.seeingfwhm = fwhm
+        self.sigma = 1.035/FWHM2SIGMA*fwhm
+        self.sigma2 = 2.*self.sigma
+        
+        ## Load VK datapoints
+        vk_atm = np.loadtxt('data/r_vonK_Kolm.txt')
+        
+        ## Extend to negative
+        vk_x = np.zeros(2*vk_atm.shape[0]-1)
+        vk_x[500:] = vk_atm[:, 0]
+        vk_x[:500] = -vk_atm[:0:-1, 0]
+        vk_y = np.zeros(2*vk_atm.shape[0]-1)
+        vk_y[500:] = vk_atm[:, 1]
+        vk_y[:500] = vk_atm[:0:-1, 1]
+        
+        self.f = interp1d(vk_x*self.seeingfwhm, vk_y, kind='cubic')
+        
+        if scale is None:
+            scale = np.arange(-4*self.sigma2, 4*self.sigma2, res)
+            obj = self.f(scale)
+        else:
+            scale = scale
+            obj = self.f(scale)
+        ConvolutionObject.__init__(self, obj, scale)
